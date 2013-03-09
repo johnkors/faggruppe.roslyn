1d9a86fa870e3e46caa7cccbd441e957391a39f4
John Korsnes <johnkors@gmail.com>
2013-03-09 16:48:54 +0100

    Init git repo

---
+++ Faggruppe.MyBusiness.CodeStandardTests/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Roslyn.Services;
+
+namespace Faggruppe.MyBusiness.CodeStandardTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private IProject _projectUnderTest;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            IWorkspace workspace;
+            workspace = Workspace.LoadSolution(@"..\..\..\Faggruppe.Roslyn.sln");
+            ISolution solution = workspace.CurrentSolution;
+            var foundProjectsByName = solution.GetProjectsByName("Faggruppe.MyBusiness");
+            _projectUnderTest = foundProjectsByName.FirstOrDefault();
+        }
+
+        [TestMethod]
+        public void Solution_ShouldContainBusinessProject()
+        {
+            Assert.IsNotNull(_projectUnderTest);
+        }
+
+        [TestMethod]
+        public void BusinessProject_EachFile_ContainsAtMostOneClass()
+        {
+            var classTokenKind = 8325;
+            var documents = _projectUnderTest.Documents;
+            foreach (var doc in documents)
+            {
+                var syntaxTree = doc.GetSyntaxTree();
+                var root = syntaxTree.GetRoot();
+                var allTokensInDocument = root.DescendantTokens();
+
+               
+                var numberOfClassKeyWordsInDoc = allTokensInDocument.Count(token => token.Kind == classTokenKind);
+
+                var errorMsg = string.Format("Document {0} contains more than 1 class. Number of class keywords usage {1}", doc.Name, numberOfClassKeyWordsInDoc);
+                Assert.IsTrue(numberOfClassKeyWordsInDoc < 2, errorMsg);
+            }
+        }
+    }
+}
---
+++ Faggruppe.MyBusiness/Person.cs
@@ -0,0 +1,32 @@
+﻿namespace Faggruppe.MyBusiness
+{
+    public class Person
+    {
+        private readonly AdresseService adresseService;
+        public Person()
+        {
+            //this.adresseService = new AdresseService();
+        }
+
+        public long Id { get; set; }
+
+        public Adresse HentAdresse()
+        {
+            return adresseService.HentAdresse(Id);
+        }
+    }
+
+    public class AdresseService
+    {
+        public Adresse HentAdresse(long personId)
+        {
+            return new Adresse();
+        }
+    }
+
+    public class Adresse
+    {
+        public string Gate { get; set; }
+        public string Postnummer { get; set; }
+    }
+}
